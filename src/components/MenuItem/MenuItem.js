08df5444f1bce765c8a6724d6357b0968c87df06
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-01 16:37:55 +0500

    Added animations & background

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -3,12 +3,13 @@ import React from "react";
 import "./MenuItem.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    className={`${size} menu-item`}
-    style={{ backgroundImage: `url(${imageUrl})` }}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
     <div className="content">
-      <h2 className="title">{title}</h2>
+      <h2 className="title">{title.toUpperCase()}</h2>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
