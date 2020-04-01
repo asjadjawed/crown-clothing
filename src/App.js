d1c17e2269c31c178083ec5e9f4aed08a82f8133
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-01 18:01:04 +0500

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact component={Homepage} path="/"></Route>
+      </Switch>
     </div>
   );
 }
