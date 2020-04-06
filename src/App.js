b3ec7706cf09e167ad536eeaba267c69e9590f45
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-06 15:11:52 +0500

    App padding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import "./App.scss";
+
 import Homepage from "./pages/Homepage/Homepage";
 import Shop from "./pages/Shop/Shop";
 
