c85fbe94669511056c29df7ba7bb3a31e06242f6
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-08 01:04:15 +0500

    Added Sign In & Sign Out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { auth } from "./firebase/firebase";
 
 import "./App.scss";
 
@@ -8,17 +9,35 @@ import Homepage from "./pages/Homepage/Homepage";
 import Shop from "./pages/Shop/Shop";
 import SignInRegister from "./pages/SignInRegister/SignInRegister";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact component={Homepage} path="/"></Route>
-        <Route exact component={Shop} path="/shop"></Route>
-        <Route exact component={SignInRegister} path="/signin"></Route>
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentUser: null,
+  };
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) =>
+      this.setState({ currentUser: user })
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact component={Homepage} path="/"></Route>
+          <Route exact component={Shop} path="/shop"></Route>
+          <Route exact component={SignInRegister} path="/signin"></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
