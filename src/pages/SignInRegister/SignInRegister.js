761ec1178103de1dd556df21c59cf4a685e1925e
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-07 15:55:33 +0500

    Added SignIn & Register page

---
+++ src/pages/SignInRegister/SignInRegister.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./SignInRegister.scss";
+
+import SignIn from "../../components/SignIn/SignIn";
+
+const SignInRegister = () => {
+  return (
+    <div className="sign-in-register">
+      <SignIn></SignIn>
+    </div>
+  );
+};
+
+export default SignInRegister;
