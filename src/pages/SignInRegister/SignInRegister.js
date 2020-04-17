0255d34b7b81c04ea3b7749c14d7a2b947870916
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-17 15:01:43 +0500

    Added Sign Up form

--- src/pages/SignInRegister/SignInRegister.js
+++ src/pages/SignInRegister/SignInRegister.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./SignInRegister.scss";
 
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 const SignInRegister = () => {
   return (
     <div className="sign-in-register">
       <SignIn></SignIn>
+      <SignUp></SignUp>
     </div>
   );
 };
