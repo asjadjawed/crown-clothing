18797ce0181a2e36ed687ee2c47462f6e60a5843
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-17 13:31:40 +0500

    Create user in Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -20,7 +20,29 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const user = await userRef.get();
+  if (!user.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("Error creating user", error);
+    }
+
+    return userRef;
+  }
+};
 
 export default firebase;
