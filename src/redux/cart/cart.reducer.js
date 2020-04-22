f457b88a8ebab8c2d023855ae736694f0a1f0eb7
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-22 21:39:56 +0500

    Adding cart to redux

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,19 @@
+import { TOGGLE_CART_HIDDEN } from "./cart.types.js";
+
+const INITIAL_CART_STATE = {
+  hidden: true,
+};
+
+const cartReducer = (state = INITIAL_CART_STATE, action) => {
+  switch (action.type) {
+    case TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
