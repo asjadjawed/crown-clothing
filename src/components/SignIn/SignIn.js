fde2f6f2cdb66429fc284fce660e2d3246c684c8
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-22 11:11:10 +0500

    Sign in with user/password added

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,7 +5,7 @@ import "./SignIn.scss";
 import FormInput from "../FormInput/FormInput";
 import CustomButton from "../CustomButton/CustomButton";
 
-import { signInWithGoogle } from "../../firebase/firebase";
+import { signInWithGoogle, auth } from "../../firebase/firebase";
 
 export class SignIn extends Component {
   state = {
@@ -13,9 +13,16 @@ export class SignIn extends Component {
     password: "",
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
